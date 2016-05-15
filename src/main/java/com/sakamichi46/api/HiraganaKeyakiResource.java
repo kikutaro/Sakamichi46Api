9bcc49e572c7dfbb175058364b398abe0b7d3716
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-05-15 23:54:53 +0900

    Add new resource for Hiraganakeyaki.

---
+++ src/main/java/com/sakamichi46/api/HiraganaKeyakiResource.java
@@ -0,0 +1,33 @@
+package com.sakamichi46.api;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import static com.sakamichi46.api.AbstractSakamichi46Resource.mapper;
+import com.sakamichi46.model.Member;
+import java.io.IOException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.ws.rs.Path;
+
+/**
+ *
+ * @author kikuta
+ */
+@Startup
+@Singleton
+@Path("hiraganakeyaki")
+public class HiraganaKeyakiResource extends AbstractSakamichi46Resource {
+    
+    @PostConstruct
+    @Override
+    public void init() {
+        try {
+            memberMap = mapper.readValue(getClass().getClassLoader().getResource("Hiraganakeyaki.json"), new TypeReference<Map<String, Member>>() {});
+        } catch (IOException ex) {
+            Logger.getLogger(HiraganaKeyakiResource.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}

8fb33876aa8dcfcf4edf28ef687b5dfb17af4a76
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-05-27 23:42:35 +0900

    Modify date format of json

--- src/main/java/com/sakamichi46/model/Member.java
+++ src/main/java/com/sakamichi46/model/Member.java
@@ -1,5 +1,6 @@
 package com.sakamichi46.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Getter @Setter
 public class Member {
     private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date birthday;
     private String bloodType;
     private String constellation;

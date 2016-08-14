b4f8821f583f73cbdabbbf64a782d8a7cb0b1b38
Yoichi Kikuta <kikutaro526@yahoo.co.jp>
2016-08-15 02:22:48 +0900

    Add Model "Music" and "Single" for new feature related music discography.

---
+++ src/main/java/com/sakamichi46/model/Music.java
@@ -0,0 +1,21 @@
+package com.sakamichi46.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author kikuta
+ */
+@Getter @Setter
+public class Music {
+    private String title;
+    private int releaseNumber;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date releaseDate;
+    private String lyricsUri;
+    private String coverPhotoUri;
+    private String officialMovieUri;
+}
---
+++ src/main/java/com/sakamichi46/model/Single.java
@@ -0,0 +1,15 @@
+package com.sakamichi46.model;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author kikuta
+ */
+@Getter @Setter
+public class Single {
+    private String title;
+    private List<Music> singleMusics;
+}
